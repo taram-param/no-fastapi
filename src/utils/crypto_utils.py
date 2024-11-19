9ee7d978f0a11b14f98048f7513e764fd0dd407b
Taram Param <ekdzhumagulov@gmail.com>
2024-11-20 04:34:30 +0600

    lint

--- src/utils/crypto_utils.py
+++ src/utils/crypto_utils.py
@@ -1,12 +1,13 @@
-import bcrypt
 import base64
+
+import bcrypt
 
 
 def hash_password(password: str) -> str:
     pwd_bytes = password.encode("utf-8")
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password=pwd_bytes, salt=salt)
-    encoded_hashed_password = base64.b64encode(hashed_password).decode('utf-8')
+    encoded_hashed_password = base64.b64encode(hashed_password).decode("utf-8")
     return encoded_hashed_password
 
 
